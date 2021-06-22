9f27c2250499754d6b21fad33773f956ef075407
Zachary-Rude <84681078+Zachary-Rude@users.noreply.github.com>
2021-06-22 16:54:45 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,5 +4,14 @@ clear
 while :
 do
   read -p '~$ ' cmd
-  eval "$cmd"
+  if [ "$cmd" = "python" ]; then
+    python2
+  else
+    if [ "$cmd" = "exit" ]; then
+      echo exit
+      break
+    else
+      eval "$cmd"
+    fi
+  fi
 done
